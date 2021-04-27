5ef6bd403883a0e8c0cd5e757382be1f0f686197
sohm-g <77761242+sohm-g@users.noreply.github.com>
2021-04-26 22:29:51 -0700

    Add files via upload

---
+++ MAIN.js
@@ -0,0 +1,170 @@
+function addition() {
+    var result =
+    Number(document.getElementById("firstno").value) +
+    Number(document.getElementById("secondno").value);
+    window.alert(result);
+}
+function sub() {
+    var result =
+    Number(document.getElementById("first").value) -
+    Number(document.getElementById("second").value);
+    window.alert(result);
+}
+function mult() {
+    var result =
+    Number(document.getElementById("first1").value) *
+    Number(document.getElementById("second2").value);
+    window.alert(result);
+}
+function div() {
+    var result =
+    Number(document.getElementById("first11").value) /
+    Number(document.getElementById("second22").value);
+    window.alert(result);
+}
+function cdpie() {
+    var result =
+    Number(document.getElementById("first111").value) *
+    3.141592653589793238
+    window.alert(result);
+}
+function crpie() {
+    var result =
+    (Number(document.getElementById("first1111").value)*2) *
+    3.141592653589793238
+    window.alert(result);
+}
+function dcpie() {
+    var result =
+    Number(document.getElementById("first11111").value) /
+    3.141592653589793238
+    window.alert(result);
+}
+function dr() {
+    var result =
+    Number(document.getElementById("first111111").value) *
+    2
+    window.alert(result);
+}
+function rcpie() {
+    var result =
+    (Number(document.getElementById("first1111111").value)/3.141592653589793238)/2
+    window.alert(result);
+}
+function rd() {
+    var result =
+    Number(document.getElementById("first11111111").value)/2
+    window.alert(result);
+}
+function apiersquare() {
+    var result =
+    Math.pow(Number(document.getElementById("first111111111").value), 2) *
+    3.141592653589793238
+    window.alert(result);
+}
+function apiedsquare() {
+    var result =
+    Math.pow((Number(document.getElementById("first111111111").value)/2), 2) *
+    3.141592653589793238
+    window.alert(result);
+}
+function square_peri() {
+    var result =
+    Number(document.getElementById("first11111111111").value)*4
+    window.alert(result);
+}
+function rectangle_peri() {
+    var result =
+    (Number(document.getElementById("first111111111111").value)*2)+(Number(document.getElementById("first222222222222").value)*2)
+    window.alert(result);
+}
+function triagle_peri() {
+    var result =
+    Number(document.getElementById("first1111111111111").value)+Number(document.getElementById("first2222222222222").value)+Number(document.getElementById("third").value)
+    window.alert(result);
+}
+function square_area() {
+    var result =
+    Math.pow(Number(document.getElementById("first11111111111111").value) , 2)
+    window.alert(result);
+}
+function rect_area() {
+    var result =
+    Number(document.getElementById("first111111111111111").value)*Number(document.getElementById("first22222222222222").value)
+    window.alert(result);
+}
+function para_area() {
+    var result =
+    Number(document.getElementById("first1111111111111111").value)*Number(document.getElementById("first222222222222222").value)
+    window.alert(result);
+}
+function tri_area() {
+    var result =
+    0.5*(Number(document.getElementById("first11111111111111111").value)*Number(document.getElementById("first2222222222222222").value))
+    window.alert(result);
+}
+function cube_vol() {
+    var result =
+    Math.pow(Number(document.getElementById("first111111111111111111").value) , 3)
+    window.alert(result);
+}
+function rrp_vol() {
+    var result =
+    Number(document.getElementById("first1111111111111111111").value)*Number(document.getElementById("first22222222222222222").value)*Number(document.getElementById("third1").value)
+    window.alert(result);
+}
+function pc_vol() {
+    var result =
+    Number(document.getElementById("first11111111111111111111").value)*Number(document.getElementById("first222222222222222222").value)
+    window.alert(result);
+}
+function pyco() {
+    var result =
+    0.33*(Number(document.getElementById("first111111111111111111111").value)*Number(document.getElementById("first2222222222222222222").value))
+    window.alert(result);
+}
+function cube_vol() {
+    var result =
+    1.33*(3.14*(Math.pow(Number(document.getElementById("first1111111111111111111111").value) , 3)))
+    window.alert(result);
+}
+function dp() {
+    var result =
+    Number(document.getElementById("first111111111111111111111111111111").value)*100
+    window.alert(result);
+}
+function fd() {
+    var result =
+    Number(document.getElementById("first11111111111111111111111").value)/Number(document.getElementById("first22222222222222222222").value)
+    window.alert(result);
+}
+function pd() {
+    var result =
+    Number(document.getElementById("first111111111111111111111111").value)/100
+    window.alert(result);
+}
+function fp() {
+    var result =
+    (Number(document.getElementById("first1111111111111111111111111").value)/Number(document.getElementById("first222222222222222222222").value))*100
+    window.alert(result);
+}
+function power() {
+    var result =
+    Math.pow(Number(document.getElementById("first11111111111111111111111111").value),Number(document.getElementById("first2222222222222222222222").value))
+    window.alert(result);
+}
+function sqrt() {
+    var result =
+    Math.sqrt(Number(document.getElementById("first111111111111111111111111111").value))
+    window.alert(result);
+}
+function cbrt() {
+    var result =
+    Math.cbrt(Number(document.getElementById("first1111111111111111111111111111").value))
+    window.alert(result);
+}
+function round() {
+    var result =
+    Math.round(Number(document.getElementById("first11111111111111111111111111111").value))
+    window.alert(result);
+}
